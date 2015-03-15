6b227d63627ad27731c08f63b0ad489d56970d70
Yannick Loriot <yannick.loriot@gmail.com>
2015-03-15 15:34:20 +0100

    fixing glitches

--- Cocarde/EqualizerLayer.swift
+++ Cocarde/EqualizerLayer.swift
@@ -1,28 +1,28 @@
 /*
- * Cocarde
- *
- * Copyright 2015-present Yannick Loriot.
- * http://yannickloriot.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
+* Cocarde
+*
+* Copyright 2015-present Yannick Loriot.
+* http://yannickloriot.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*/
 
 import Foundation
 import UIKit
@@ -65,16 +65,20 @@ internal final class EqualizerLayer: CocardeLayer {
     var values: [CGFloat] = []
     
     for i in 0 ..< segmentCount {
-      let plotScale = (plotMaxScale - plotMinScale) * CGFloat(sin(M_PI / Double(segmentCount) * Double(i)))
+      var plotScale: CGFloat = 0
+      
+      if i < segmentCount / 2 {
+        plotScale = (plotMaxScale - plotMinScale) * CGFloat(sin(M_PI / Double(segmentCount / 2) * Double(i)))
+      }
       
       values.append(plotMinScale + plotScale)
     }
     
-    for i in 0 ..< 1 {
+    for i in 0 ..< 2 {
       values.insert(plotMinScale, atIndex: 0)
       values.append(plotMinScale)
     }
-
+    
     for i in 0 ..< segmentCount {
       let startAngle = CGFloat(i) * angle
       let endAngle   = startAngle + angle
@@ -83,7 +87,7 @@ internal final class EqualizerLayer: CocardeLayer {
       addSublayer(plotLayer)
       
       let plotRect = CGRectMake(-CGRectGetWidth(rect) / 2 + plotWidth * CGFloat(i) + plotGapSize / 2, -plotHeight / 2, plotWidth - plotGapSize, plotHeight)
-
+      
       plotLayer.path        = UIBezierPath(rect: plotRect).CGPath
       plotLayer.fillColor   = segmentColors[Int(i) % colorCount].CGColor
       plotLayer.strokeColor = plotLayer.fillColor
@@ -95,7 +99,7 @@ internal final class EqualizerLayer: CocardeLayer {
       anim.repeatCount         = Float.infinity
       anim.values              = values
       anim.timeOffset          = loopDuration - (loopDuration / CFTimeInterval(segmentCount) * CFTimeInterval(i))
-      anim.timingFunction      = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+      //anim.timingFunction      = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
       anim.removedOnCompletion = false
       plotLayer.addAnimation(anim, forKey: "plot.scale")
     }
--- Example/Cocarde/DetailViewController.swift
+++ Example/Cocarde/DetailViewController.swift
@@ -25,6 +25,21 @@ class DetailViewController: UIViewController {
       title                 = style.description
       cocardeView.style     = style
       cocardeView.animating = true
+      
+      switch style {
+      case .Default:
+        cocardeView.colors       = [UIColor(hex: 0x3498db), UIColor(hex: 0xecf0f1), UIColor(hex: 0xe74c3c)]
+        cocardeView.loopDuration = 6
+        cocardeView.segmentCount = 5
+      case .Pie:
+        cocardeView.colors       = [UIColor(hex: 0xdb5c65), UIColor(hex: 0xA7405d), UIColor(hex: 0x3b1c57), UIColor(hex: 0xf59155), UIColor(hex: 0x733633)]
+        cocardeView.loopDuration = 12
+        cocardeView.segmentCount = 18
+      case .Equalizer:
+        cocardeView.colors       = [UIColor(hex: 0x2ecc71), UIColor(hex: 0x3498db), UIColor(hex: 0x9b59b6), UIColor(hex: 0xe67e22), UIColor(hex: 0xc0392b), UIColor(hex: 0xe74c3c), UIColor(hex: 0xe74c8c)]
+        cocardeView.loopDuration = 4
+        cocardeView.segmentCount = 14
+      }
     }
   }
   
